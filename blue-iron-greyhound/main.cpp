0aa6ed32340c7577eadc359d7e264044c266befd
Adam S <adamstanton96@gmail.com>
2017-11-05 11:11:28 +0000

    Revert "SDL and GLEW window set up"

--- blue-iron-greyhound/main.cpp
+++ blue-iron-greyhound/main.cpp
@@ -5,39 +5,26 @@
 commentcomment chloe
 =============================================================================*/
 
-#if _DEBUG
-#pragma comment(linker, "/subsystem:\"console\" /entry:\"WinMainCRTStartup\"")
-#endif
-
 #include "TestComponent.h"
 #include "InteractiveTestComponent.h"
-#include "IronRifts.h"
-
-
-
 
 int main(int argc, char *argv[])
 {
+	int i = 0; //test
+	GameObject *testObj = new GameObject("testObj");
+	TestComponent *testComponentA = new TestComponent("testComponentA");
+	InteractiveTestComponent *testComponentB = new InteractiveTestComponent("testComponentB");
 
-	IronRifts game;
+	testObj->addComponent(testComponentA);
+	testObj->addComponent(testComponentB);
 	
+	testObj->init();
+	testObj->update();
+	testObj->getComponent<InteractiveTestComponent>()->update();
+	testObj->update();
 
-	//GameObject *testObj = new GameObject("testObj");
-	//TestComponent *testComponentA = new TestComponent("testComponentA");
-	//InteractiveTestComponent *testComponentB = new InteractiveTestComponent("testComponentB");
-
-	//testObj->addComponent(testComponentA);
-	//testObj->addComponent(testComponentB);
-	//
-	//testObj->init();
-	//testObj->update();
-	//testObj->getComponent<InteractiveTestComponent>()->update();
-	//testObj->update();
-
-	//int i = 1;
-	//do {} while (i != 0);
-
-
+	int i = 1;
+	do {} while (i != 0);
 
 	return 0;
 }
