47237b42afe431a6598035a9771c198bc080b3db
ChloeMadden <chloe_madden@live.com>
2017-11-05 14:53:08 +0000

    Dont know whats happening

--- blue-iron-greyhound/main.cpp
+++ blue-iron-greyhound/main.cpp
@@ -11,7 +11,9 @@ commentcomment chloe
 
 #include "TestComponent.h"
 #include "InteractiveTestComponent.h"
-#include "IronRifts.h"
+#include "openglRenderer.h"
+
+#include "SDL.h"
 
 
 
@@ -19,23 +21,34 @@ commentcomment chloe
 int main(int argc, char *argv[])
 {
 
-	IronRifts game;
+	//IronRifts game;
+	RenderingSystem* renderer = new openglRenderer();
+
+	/*GameObject *testObj = new GameObject("testObj");
+	TestComponent *testComponentA = new TestComponent("testComponentA");
+	InteractiveTestComponent *testComponentB = new InteractiveTestComponent("testComponentB");
+
+	testObj->addComponent(testComponentA);
+	testObj->addComponent(testComponentB);
 	
+	testObj->init();
+	testObj->update();
+	testObj->getComponent<InteractiveTestComponent>()->update();
+	testObj->update();
+	*/
+	int i = 1;
+	
+	SDL_Event sdlEvent;
+	do 
+	{
+		while (SDL_PollEvent(&sdlEvent)) {				//This poll event should not be here since it couples the main to SDL. 
+			if (sdlEvent.type == SDL_QUIT)
+				i = 0;
+		}
 
-	//GameObject *testObj = new GameObject("testObj");
-	//TestComponent *testComponentA = new TestComponent("testComponentA");
-	//InteractiveTestComponent *testComponentB = new InteractiveTestComponent("testComponentB");
-
-	//testObj->addComponent(testComponentA);
-	//testObj->addComponent(testComponentB);
-	//
-	//testObj->init();
-	//testObj->update();
-	//testObj->getComponent<InteractiveTestComponent>()->update();
-	//testObj->update();
-
-	//int i = 1;
-	//do {} while (i != 0);
+		renderer->draw();
+	
+	} while (i != 0);
 
 
 
